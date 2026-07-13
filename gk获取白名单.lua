2242d90932583ea9d6e8a672b2aeca81b4517f81
GKye9178 <3829174015@qq.com>
2026-07-13 21:13:36 +0800

    Fix formatting in gk获取白名单.lua

--- gk获取白名单.lua
+++ gk获取白名单.lua
@@ -2,7 +2,7 @@
     "mode": false,
     "devices":[
         "2cd0ae839dd0792499eef5ceea7c53d7bca92edf0e0603db876d9457b4e5ea90", 
-        "235fd33ec94036f7", --2071919635
+        "235fd33ec94036f7", 
         "65b0ee1636b7d1db65522fbe0e87ce994937a0572d9834aee3af5b19d3e89a42", 
         "79f39a629998ef14ba401c261cdb17a1787c43499f1c7fc97a44606c8b593692", 
         "",
